01b45f847b32ed88c7a194b7e5398a0436793eec
JOSE DAVID CAMACHO VARGAS <jose.camachovargas@ucr.ac.cr>
2021-11-23 13:33:15 -0600

    Creación código base

---
+++ Operaciones.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorajava;
+
+/**
+ *
+ * @author Jose David
+ */
+public class Operaciones {
+
+    public Operaciones() {
+    }
+    
+    public double suma(double x, double y){
+        return 0;
+    }
+    
+    public double resta(double x, double y){
+        return 0;
+    }
+    
+    public double multiplicacion(double x, double y){
+        return 0;
+    }
+    
+    public double division(double x, double y){
+        return 0;
+    }
+}
